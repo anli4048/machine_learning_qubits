fe657f1d1ce34d49b7c7550ce6874da2b8082906
Andrew Li <anli4048@uni.sydney.edu.au>
2023-04-27 17:17:10 +1000

    Added autograd practice
    Calculating T1 with autograd

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-import scqubits.testing as sctest
-sctest.run()
+#import scqubits.testing as sctest
+#sctest.run()
+
+import numpy as np
+dimension = 4
+offdiag_elements = np.sqrt(range(1, dimension))
+print(np.diagflat(offdiag_elements, 1))
